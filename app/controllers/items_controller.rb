5eb972977d72cbbc8505ecfd1208f0e555c060da
Jon Ablondi <jonnyablondi@hotmail.com>
2017-10-20 10:51:29 +1100

    items updated with style instead of type in database

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:type, :name, :description, :image_data, :condition, :price, :user_id)
+      params.require(:item).permit(:style, :name, :description, :image_data, :condition, :price, :user_id)
     end
 end
