e2e6fcd96114eed2fbcbf483af0aaba8348533f4
Jon Ablondi <jonnyablondi@hotmail.com>
2017-10-20 11:23:27 +1100

    price column in items table updated, now decimal precision 6 scale 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019233646) do
+ActiveRecord::Schema.define(version: 20171020001331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20171019233646) do
     t.string "description"
     t.string "image_data"
     t.string "condition"
-    t.float "price"
+    t.decimal "price", precision: 6, scale: 2
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
