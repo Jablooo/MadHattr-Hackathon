fa315c3144959e048ce082abb67ecb798b9b972b
Jon Ablondi <jonnyablondi@hotmail.com>
2017-10-20 15:55:18 +1100

    styling and inserting information into payment area

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,8 +1,11 @@
 class ChargesController < ApplicationController
   def new
+    @items = Item.all
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     # Amount in cents
     @amount = 500
 
@@ -21,5 +24,19 @@ class ChargesController < ApplicationController
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(
+      :price,
+      :user_id
+    )
   end
 end
