2bf8dac2b20299e766d2023d331d24e5a96f348d
Syaf Djamil <the.syaf@gmail.com>
2017-10-20 12:18:31 +1100

    Home cards updated to pull from items table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
+    @item = Item.all
+    @profiles = Profile.all
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(
+        :style,
+        :name,
+        :description,
+        :image,
+        :condition,
+        :price,
+        :user_id
+      )
+    end
 end
